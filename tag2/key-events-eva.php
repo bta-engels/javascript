8e8f423c8deecd0f8c0d09d2e5f55df43dbdd33e
codeamazone <e.niggeme@gmail.com>
2021-01-26 15:01:12 +0100

    Update key-events-eva.php

--- tag2/key-events-eva.php
+++ tag2/key-events-eva.php
@@ -1,5 +1,23 @@
 <?php require '../inc/header.shtml'; ?>
 
-<div id="txt"></div>
+<div id="txt">Mein Text</div>
+<hr>
+<input type="text" id="inpText">
+
+
+
+<script>
+    var div = document.getElementById('txt'),
+        inp = document.getElementById('inpText');
+
+    // onkeyup = Moment, wenn Taste losgelassen wird
+    inp.onkeyup = function(e) {
+        var wert = e.target.value;
+        // console.info(e.target.value);
+        div.innerText = e.target.value;
+        console.log(wert.length);
+    }
+</script>
+
 
 <?php require '../inc/footer.shtml'; ?>
