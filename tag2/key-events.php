79929ffa0963e6049daf7499974a5db0534d48d1
berndengels <engels@f50.de>
2021-01-26 14:39:05 +0100

    stuff

---
+++ tag2/key-events.php
@@ -0,0 +1,7 @@
+<?php require '../inc/header.shtml'; ?>
+
+<div id="txt">Mein Text</div>
+<hr>
+<input type="text" id="inpText">
+
+<?php require '../inc/footer.shtml'; ?>
--- tag2/select-style.php
+++ tag2/select-style.php
@@ -4,17 +4,37 @@
 <div></div>
 
 <script>
-    var styles = ['default','darkPower','redYellow','blueRed']
-    var body = document.querySelector('body')
-    var div = document.querySelector('div')
+    var styles = ['default','darkPower','redYellow','blueRed'],
+        body = document.querySelector('body'),
+        div = document.querySelector('div'),
+        // erzeuge eine select element
+        select = document.createElement('select'),
+        item,option;
 
-    var select = document.createElement('select');
     // options über style loop erzeugen
-    var option = document.createElement('option');
-    option.innerText = "meine Option"
-
-    select.appendChild(option)
+    for(item of styles) {
+    	// erzeuge option element
+	    option = document.createElement('option');
+	    option.innerText = item // anzeige text setzen
+	    option.value = item // wert setzen
+        // hänge das option element als child ins select element
+	    select.appendChild(option)
+    }
+    // hänge mein select ins leere div element ein
     div.appendChild(select);
 
+    // onchange event für select
+    function changeStyle(e) {
+	    // setze css class name
+        console.info(e.target.value)
+	    body.className = e.target.value;
+    }
+/*
+    select.onchange = function (e) {
+	    body.className = e.target.value;
+    }
+ */
+    // oder
+    select.onchange = changeStyle
 </script>
 <?php require '../inc/footer.shtml'; ?>
