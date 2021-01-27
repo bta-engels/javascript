3550c927182e2a378ba7e29bf1a23fcaa4df1abc
berndengels <engels@f50.de>
2021-01-27 13:21:06 +0100

    wetter

--- tag3/wetter.php
+++ tag3/wetter.php
@@ -5,7 +5,11 @@
 <h3 id="beschreibung"></h3>
 
 <script>
-    var city = 'Berlin',
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',de&lang=de&units=metric&APPID='+ OW_API_KEY;
+var city = 'Berlin',
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',de&lang=de&units=metric&APPID=' + OW_API_KEY;
+
+$.get(url, function (resp) {
+    console.info(resp);
+});
 </script>
 <?php require '../inc/footer.shtml'; ?>
