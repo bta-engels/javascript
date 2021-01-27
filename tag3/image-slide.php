85b4b8f74990450c51e96c647ae393b4d42b6719
berndengels <engels@f50.de>
2021-01-27 10:40:38 +0100

    stuff

--- tag3/image-slide.php
+++ tag3/image-slide.php
@@ -1,11 +1,29 @@
 <?php require '../inc/header.shtml'; ?>
-
+<h1></h1>
 <div class="row">
-    <img src="../img/cowboy.jpg" class="align-self-center m-auto" height="500" alt="Slider" title="Slider">
+    <img id="image" src="../img/fahrrad.jpg" class="align-self-center m-auto" height="500" alt="Slider" title="Slider">
 </div>
 
 <script>
+    var counter = 0,
+        delay = 2000,
+        h1 = document.querySelector('h1'),
+        images = ['see.jpg','vw.jpg','ziel.jpg'],
+        anzahlBilder = images.length;
 
+    function slide(param = "test") {
+    	if(counter === anzahlBilder) {
+    		counter = 0;
+            // oder interval stoppen
+            //clearInterval(interval);
+            //return null;
+        }
+
+    	var img = document.getElementById('image');
+    	img.src = "../img/" + images[counter];
+    	counter++;
+    }
+    var interval = setInterval(slide, delay);
 </script>
 <?php require '../inc/footer.shtml'; ?>
 
