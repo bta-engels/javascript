4291a88a1f1a9b5f37c475f6a7866c622fc85d26
berndengels <engels@f50.de>
2021-01-27 12:22:21 +0100

    ajax

--- tag3/autoren.php
+++ tag3/autoren.php
@@ -9,7 +9,19 @@
         url = 'http://bta-movies.loc/api/authors';
     // ajax request per GET methode
     $.get(url, function(response) {
-    	console.info(response);
+	    console.info(response);
+    	h1.style.display = 'none';
+    	if(response.error) {
+    		// wenn fehler aufgetreten ist
+    		alert(response.error)
+        } else {
+    		// alles ist gut gegangen, baue hier meine liste (li) zusammen
+            var authors = response.data;
+            // li - elem bauen
+            // firstname, lastname vom autor darstellen
+            // li - elem dem ul - elem hinzufügen
+
+        }
     });
 </script>
 <?php require '../inc/footer.shtml'; ?>
